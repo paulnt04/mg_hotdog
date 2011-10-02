06d062ca035326c67af6a998a992b04cbdde73f4
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2011-10-01 22:26:44 -0700

    start specs for opening rooms

--- spec/mg_hotdog/connection_spec.rb
+++ spec/mg_hotdog/connection_spec.rb
@@ -1,7 +1,16 @@
 require 'spec_helper'
 
 module MgHotdog
-  describe "Open a room" do
-    it "should connect to campfire" 
+  describe Connection do
+    describe "Open a room" do
+      it "should require a room number" do
+        lambda { 
+          Connection.new.open
+        }.should raise_error ArgumentError
+      end
+
+      it "should return a room"
+
+    end
   end
 end
