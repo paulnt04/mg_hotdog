8853b11860bb3e32862749303a836d78939a07f7
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2011-10-02 20:48:27 -0700

    remove old listen methods from robot

--- lib/mg_hotdog/robot.rb
+++ lib/mg_hotdog/robot.rb
@@ -28,14 +28,6 @@ module MgHotdog
       end
     end
 
-    def listen(pattern, responder)
-      @parts << [pattern, responder, :body]
-    end
-
-    def listen_for(pattern, options)
-      @parts << [pattern, options[:with], options[:in]]
-    end
-
     def speak message
       @room.speak message
     end
